0a3fb353096d430f9da68a7acfbffa9be7c0fe46
Ben Wurster <wursterben17@gmail.com>
2023-01-06 16:26:29 -0600

    working minus transaction commit

--- frontend2/index.js
+++ frontend2/index.js
@@ -3,6 +3,10 @@ const bodyParser = require("body-parser")
 const fs = require('fs');
 const app = express();
 
+let accounts = {};
+let locked = true;
+
+
 // get our app to use body parser 
 app.use(bodyParser.urlencoded({ extended: true }))
 
@@ -10,30 +14,44 @@ app.listen(3000, () => {
 	console.log("Application started and Listening on port 3000");
 });
 
-app.get("/", (req, res) => {
-	res.send("<h1>Home</h1>");
+app.get("/accounts", (req, res) => {
+	res.send({ accounts: Object.keys(accounts) });
 });
 
-app.get("/data/addrs.txt", (req, res) => {
-	res.sendFile(__dirname + "/data/addrs.txt");
+app.get("/send", (req, res) => {
+	if(locked) {
+		res.sendFile(__dirname + "/pages/lock/index.html");
+	} else {
+		res.sendFile(__dirname + "/pages/send/index.html");
+	}
 });
 
-app.get("/drop", (req, res) => {
-	res.sendFile(__dirname + "/drop-page/index.html");
+app.post("/send", (req, res) => {
+	locked = true;
+	accounts = {}; 	// clear the addresses
+    res.send("Thank you for using the application!");
 });
 
-app.post("/drop", (req, res) => {
-	console.log("Request for Payment")
-    fs.writeFileSync("data/addrs.txt", "");
-    res.send("Sent?");
+app.get("/clear", (req, res) => {
+	accounts = {};
+	res.send("Cleared!")
+})
+
+app.get("/unlock", (req, res) => {
+	locked = false;
+	res.send("Unlocking!");
+})
+
+app.get("/request", (req, res) => {
+	if(locked) {
+		res.send("Application closed.")
+	} else {
+		res.sendFile(__dirname + "/pages/request/index.html");
+	}
 });
 
-app.get("/get", (req, res) => {
-	res.sendFile(__dirname + "/queue-page/index.html");
-});
-
-app.post("/get", (req, res) => {
+app.post("/request", (req, res) => {
 	let pubKey = req.body.pubKey;
-    fs.appendFileSync("data/addrs.txt", pubKey + "\n");
-    res.send("Thank you for coming to this meeting! You are now in the queue to receive your BT reward!")
+	accounts[pubKey] = 1;	// add address to map for checking
+	res.send("Thank you for coming to this meeting! You are now in the queue to receive your BT reward!")
 })
