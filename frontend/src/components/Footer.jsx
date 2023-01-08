80ba51b9243cc6925fcb4d342f5f935719fad9b1
hackerrank <hfgoff@wisc.edu>
2023-01-07 20:54:29 -0600

    admin page

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,15 +1,4 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-
-// have the FAQ be cards (3x6) that are hoverable
-// add in a light / dark mode toggle
-// /graph endpoint to see subgraphs
-// link to our twitter (like ethleaderboard.xyz)
-// have three boxes that say total unique address, total issues, total redeemed
-// add footer with website by: , github: , and intital idea: , link to wesbtie:
-
 import "./components.css";
 
 export default function Footer() {
---
+++ frontend/src/pages/Admin.jsx
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import { Container, Row, Col, Button, Modal } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+
+import "./Pages.css";
+
+export default function Admin() {
+  const navigate = useNavigate();
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  function handleClick() {
+    navigate("/");
+  }
+
+  function handleAirdrop() {}
+
+  const metrics = [
+    {
+      id: 1,
+      title: "total tokens to airdrop",
+      url: "24",
+    },
+    {
+      id: 2,
+      title: "total requesters / attendees",
+      url: "12",
+    },
+    {
+      id: 3,
+      title: "total tokens in cirulation",
+      url: "10089",
+    },
+    {
+      id: 4,
+      title: "total unique addresses",
+      url: "67",
+    },
+    {
+      id: 5,
+      title: "average attendees",
+      url: "22",
+    },
+  ];
+
+  return (
+    <Container fluid>
+      <Row>
+        <Col md={12}>
+          <div class="page-header">
+            <h1 className="header">Admin Dashboard</h1>
+            <Button className="home-btn" onClick={handleClick}>
+              Main Page
+            </Button>
+            <Button className="home-btn" variant="danger" onClick={handleShow}>
+              Airdrop Token
+            </Button>
+          </div>
+        </Col>
+      </Row>
+      <Row className="">
+        {metrics.map((metric, index) => (
+          <Col key={metric.id}>
+            <div className="metrics-card">
+              <h3 className="metrics-title">{metric.title}</h3>
+              <p className="metrics-numbers">{metric.url}</p>
+            </div>
+          </Col>
+        ))}
+      </Row>
+      <Row>
+        <Col md={8}></Col>
+        <Col md={4}></Col>
+      </Row>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Confirm Airdrop</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Are you sure you want to airdrop "num_tokens" to "num_addresses"{" "}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            No
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Yes, Airdrop
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </Container>
+  );
+}
