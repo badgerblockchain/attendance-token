87425679c7f13784220bdf7740b2dee3f45dc6e1
Ben Wurster <wursterben17@gmail.com>
2023-02-05 20:28:41 -0600

    contract connection success

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -9,6 +9,10 @@ import { collection, getDocs, deleteDoc } from "firebase/firestore";
 // material ui imports
 //import { Card, CardActions, CardContent, Typography } from "@mui/material";
 
+// ethers import
+import { ethers } from "ethers";
+
+
 import DataTableAll from "../components/DataTableAll";
 import DataTableQueue from "../components/DataTableQueue";
 import Alert from "../components/Alert";
@@ -18,41 +22,42 @@ import "./Pages.css";
 export default function Admin() {
   const navigate = useNavigate();
   const [show, setShow] = useState(false);
+  const [account, setAccount] = useState(0);
   const [addresses, setAddresses] = useState([]);
   const [authorized, setAuthorized] = useState(false);
   const addressCollectionRef = collection(db, "addresses");
   const totalAddressCollectionRef = collection(db, "all-addresses-ever");
-  const whitelistAddresses = ["0xf78d0E652D3471cda42E02B6D0964820F87A8126"].map(
+  const whitelistAddresses = ["0x123547171488c7CE7c811127eF7452B74E20D9D9"].map(
     (v) => v.toLowerCase()
   );
   const { ethereum } = window;
   const [size, setSize] = useState(0);
   const [uniqueSize, setUniqueSize] = useState(0);
 
-  const determineAuthorized = async () => {
-    const accountAddr = await window.ethereum.request({
-      method: "eth_requestAccounts",
-    });
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = provider.getSigner()
+  const contractAddress = "0xEFf3485745a32a93788eB7cC249A534F77682522";
+  const ERC20_ABI = [
+    "function transfer(address _to, uint256 _value) public returns (bool success)",
+    "function bulkTransfer(address[] memory addresses, uint tokensEach) public returns (bool success)",
+  ]
+  const contract = new ethers.Contract(contractAddress, ERC20_ABI, provider);
 
-    console.log("acccountAddr:", accountAddr[0]);
-    console.log(whitelistAddresses[0]);
+  const determineAuthorized = async () => {
+    const accountAddr = await provider.send("eth_requestAccounts", []);
+
     if (whitelistAddresses.includes(accountAddr[0])) {
-      console.log("you are authorized with:", accountAddr[0]);
       setAuthorized(true);
     }
-    console.log("you are not authorized with:", accountAddr[0]);
   };
 
   useEffect(() => {
     const getAddresses = async () => {
       const data = await getDocs(addressCollectionRef);
-      //console.log(data);
-      setAddresses(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      setAddresses(data.docs.map((doc) => doc.data().address));
+
       const querySnapshot = await getDocs(addressCollectionRef);
-      querySnapshot.forEach((doc) => {
-        // console.log(doc.id, " => ", doc.data());
-        setAddresses(doc.data());
-      });
+      
       setSize(querySnapshot.size);
       const querySnapshot2 = await getDocs(totalAddressCollectionRef);
       setUniqueSize(querySnapshot2.size);
@@ -69,8 +74,12 @@ export default function Admin() {
     navigate("/");
   }
 
-  function handleAirdrop() {
-    console.log("they really want to airdrop");
+  const handleAirdrop = async () => {
+    console.log("Starting airdrop");
+    const contractWithWallet = contract.connect(signer);
+    const tx = await contractWithWallet.bulkTransfer(addresses, ethers.utils.parseUnits((size/addresses.length).toString(), "ether")); 
+    await tx.wait();
+    console.log("Airdrop complete!");
     setShow(false);
   }
 
